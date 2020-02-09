08da01f38971669e0e6aad73b3da999fbc7f0f25
TannakornM <60841481+TannakornM@users.noreply.github.com>
2020-02-09 22:11:56 +0700

    can choose pokemon Starter

--- Pokemon.java
+++ Pokemon.java
@@ -12,7 +12,66 @@ public class Pokemon{
         
         return " " ;
     }
+    //=====================Pokemon Starter=========================
+    public String charmander(){
+        String namemon1 = "charmande";
+
+        int   hp1 = 86;
+        int   cp1 = 102;
+        System.out.println(namemon1);
+        System.out.println("HP : "+hp1);
+        System.out.println("CP : "+cp1);
+        return " ";
+     }
+
+     public String Squirtle(){
+        String namemon2 = "Squirtle";
+        int   hp2 = 84;
+        int   cp2 = 105;
+        System.out.println(namemon2);
+        System.out.println("HP : "+hp2);
+        System.out.println("CP : "+cp2);
+        return " ";
+     }
+
     
-    
+     public String Pikachu(){
+        String namemon3 = "Pikachu";
+        int   hp3 = 102;
+        int   cp3 = 110;
+        System.out.println(namemon3);
+        System.out.println("HP : "+hp3);
+        System.out.println("CP : "+cp3);
+        return " ";
+     }
+    //============================================================
+    public String choosePokemon(){
+        int threepokemon[];
+        int x;
+        System.out.println("Choose Your Pokemon");
+            System.out.println("\t1. Charmander");
+            System.out.println("\t2. Squirtle");
+            System.out.println("\t3. Pikachu");
+
+        Scanner in1 = new Scanner(System.in);
+        System.out.print("Enter Pokemon (1,2,3) : ");
+        x = in1.nextInt();
+        if(x == 1){
+            //System.out.println("NAME"+setName(name));
+            charmander();
+          }
+        else if(x == 2){
+            //System.out.println("NAME"+setName(name));
+            Squirtle();
+          }
+        else if(x == 3){
+            //System.out.println("NAME"+setName(name));
+            Pikachu();
+          }
+        else {
+            System.out.println("Error"); 
+        }
+        return " ";    
+    }
     
 }
--- PokemonGame.java
+++ PokemonGame.java
@@ -1,11 +1,10 @@
 import java.io.*;
 public class PokemonGame{
   public static void main( String[] args ){
-    int i ;
       Pokemon user = new Pokemon() ;  //call the method from Pokemon.java
       System.out.println(" Welcome  Pokemon Go ");
       user.setName();
-      //user.startGame();
+      user.choosePokemon();
       
   }
 } 
