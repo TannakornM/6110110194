9d74a771e17a9eb0c46741378632876660ad800a
TannakornM <60841481+TannakornM@users.noreply.github.com>
2020-02-09 20:40:33 +0700

    upload New file

---
+++ Pokemon.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.Random;
+
+public class Pokemon{
+
+    public String setName(){
+        String name;
+        final Scanner in = new Scanner(System.in);
+        System.out.print("Enter your name :");
+        name = in.next();
+        
+        return " " ;
+    }
+    
+    
+    
+}
